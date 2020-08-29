6513c355c175e5f3edfa79365ea3843faed103f3
Jack <jackmurphy756@gmail.com>
2020-08-29 23:51:41 +0100

    add prettier package to dev deps

--- server/src/server.test.ts
+++ server/src/server.test.ts
@@ -1,7 +1,11 @@
 const request = require("supertest");
+const app = require("./app");
 
 describe("Server setup", () => {
-  it("Dummy test", () => {
+  it("is just testing.", () => {
     expect(1).toBe(1);
+  });
+  it("expects to connect with a DB", () => {
+
   });
 });
